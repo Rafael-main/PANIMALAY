196f717d53ea37129381ca10654965f10ebd0511
Rafael-main <rafaeladrian.tibudan1201@g.msuiit.edu.ph>
2022-02-20 00:17:02 +0800

    all over the place now...

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// css framework oruga
+import Oruga from '@oruga-ui/oruga-next'
+import '@oruga-ui/oruga-next/dist/oruga.css'
+
 import App from './App.vue'
 import router from './router'
 
@@ -8,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Oruga)
 
 app.mount('#app')
